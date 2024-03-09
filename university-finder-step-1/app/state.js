a8b78dcc592d009f06fc8862bf8648752f55cd37
Matt Driscoll <mdriscoll@esri.com>
2024-03-09 14:33:44 -0800

    separate steps

---
+++ university-finder-step-1/app/state.js
@@ -0,0 +1,12 @@
+export const appState = {
+  count: 0,
+  activeItem: false,
+  savedExtent: null,
+  hasFilterChanges: false,
+  attendance: { min: 0, max: 160000 },
+  housing: { enabled: false, min: 0, max: 20000 },
+  activeProgramTypes: [],
+  filterOpen: true,
+  smallBreakpoint: false,
+  theme: "light",
+};
